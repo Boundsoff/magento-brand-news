426d673e1cc0af46252160550fd3befc9d93d286
Kamil Wiśniowski <task-discus-0a@icloud.com>
2025-04-04 21:39:42 +0200

    add exception for cron run smoothly

--- Cron/BlogFeedConsumer.php
+++ Cron/BlogFeedConsumer.php
@@ -7,6 +7,7 @@ use Boundsoff\BrandNews\Model\ConfigEnableOptions;
 use Boundsoff\BrandNews\Model\Exception\BlogFeedConsumerException;
 use Magento\Framework\FlagManager;
 use Magento\Framework\Stdlib\DateTime\TimezoneInterface;
+use Psr\Log\LoggerInterface;
 
 class BlogFeedConsumer
 {
@@ -16,11 +17,13 @@ class BlogFeedConsumer
      * @param FeedbackServiceInterface $feedbackService
      * @param FlagManager $flagManager
      * @param TimezoneInterface $timezone
+     * @param LoggerInterface $logger
      */
     public function __construct(
         protected readonly FeedbackServiceInterface $feedbackService,
         protected readonly FlagManager              $flagManager,
         protected readonly TimezoneInterface        $timezone,
+        protected readonly LoggerInterface          $logger,
     ) {
     }
 
@@ -28,30 +31,32 @@ class BlogFeedConsumer
      * Consume feed and add ass notification
      *
      * @return void
-     * @throws BlogFeedConsumerException
-     * @throws \Boundsoff\BrandNews\Model\Exception\FeedbackServiceException
-     * @throws \DateMalformedStringException
      */
     public function execute(): void
     {
-        if (!$this->feedbackService->isEnabled(ConfigEnableOptions::BlogFeed)) {
-            return;
+        try {
+            if (!$this->feedbackService->isEnabled(ConfigEnableOptions::BlogFeed)) {
+                return;
+            }
+
+            $timestamp = $this->flagManager->getFlagData(static::TIMESTAMP_FEED_LAST_UPDATE_FLAG_CODE);
+            if (!empty($timestamp)) {
+                $timestamp = $this->timezone->date($timestamp, useTimezone: false, includeTime: false);
+            }
+
+            $entries = $this->feedbackService->readBlogFeed($timestamp);
+            array_walk($entries, fn($entry) => $this->feedbackService->addAdminNotification(
+                $entry->getTitle(),
+                $entry->getDescription(),
+                $entry->getLink(),
+            ));
+
+            $timestamp = $this->timezone->date(useTimezone: false, includeTime: false)
+                ->getTimestamp();
+            $this->flagManager->saveFlag(static::TIMESTAMP_FEED_LAST_UPDATE_FLAG_CODE, $timestamp);
+        } catch (\Throwable $exception) {
+            $this->logger->error($exception->getMessage());
+            $this->logger->debug($exception->getTraceAsString());
         }
-
-        $timestamp = $this->flagManager->getFlagData(static::TIMESTAMP_FEED_LAST_UPDATE_FLAG_CODE);
-        if (!empty($timestamp)) {
-            $timestamp = $this->timezone->date($timestamp, useTimezone: false, includeTime: false);
-        }
-
-        $entries = $this->feedbackService->readBlogFeed($timestamp);
-        array_walk($entries, fn($entry) => $this->feedbackService->addAdminNotification(
-            $entry->getTitle(),
-            $entry->getDescription(),
-            $entry->getLink(),
-        ));
-
-        $timestamp = $this->timezone->date(useTimezone: false, includeTime: false)
-            ->getTimestamp();
-        $this->flagManager->saveFlag(static::TIMESTAMP_FEED_LAST_UPDATE_FLAG_CODE, $timestamp);
     }
 }
