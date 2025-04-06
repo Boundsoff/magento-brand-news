3aa35f4fa43b4a3b1c11307539b758b46adbaba5
Kamil Wiśniowski <task-discus-0a@icloud.com>
2025-04-06 19:14:22 +0200

    small fixes

--- Block/Adminhtml/Marketplace.php
+++ Block/Adminhtml/Marketplace.php
@@ -3,12 +3,13 @@
 namespace Boundsoff\BrandNews\Block\Adminhtml;
 
 use Boundsoff\BrandNews\Api\FeedbackServiceInterface;
+use Boundsoff\BrandNews\Api\ModuleInfoServiceInterface;
 use Boundsoff\BrandNews\Helper\Data as Helper;
 use Boundsoff\BrandNews\Model\ConfigEnableOptions;
 use Magento\Backend\Block\Template;
 
 /**
- * @method FeedbackServiceInterface getFeedbackService()
+ * @method ModuleInfoServiceInterface getModuleInfoService()
  * @method string getMarketplaceUri()
  * @method Helper getDataHelper()
  */
